2f11b7516b692c2484d47758145ac8bc22aaebcc
allisonnevias <allison.h.nevias@vanderbilt.edu>
2023-09-27 19:11:19 -0500

    Flight Info Front End - Put in date

--- src/Components/FlightInfo/FlightInfo.js
+++ src/Components/FlightInfo/FlightInfo.js
@@ -3,26 +3,26 @@ import "./FlightInfo.css";
 
 function FlightInfo() {
     const [time, setTime] = useState('');
+    const [date, setDate] = useState('');
     const [dropoff, setDropoff] = useState('');
     const [airline, setAirline] = useState('');
-    const [option, setOption] = useState('');  // New state to handle dropdown selection
+    const [option, setOption] = useState('');  
   
     const handleSubmit = () => {
-      // TODO: handle form submission based on the selected option
     };
   
     return (
       <div className="flight-registration-container">
         <h1>Flight Registration</h1>
   
-        {/* New Dropdown */}
         <select value={option} onChange={e => setOption(e.target.value)}>
-        <option value="" disabled>Select Type of Ride</option> {/* New default option */}
+        <option value="" disabled>Select Type of Ride</option>
         <option value="dropoff">Dropoff</option>
         <option value="pickup">Pickup</option>
       </select>
         
         <input type="text" placeholder="Flight Time" value={time} onChange={e => setTime(e.target.value)} />
+        <input type="date" placeholder="Flight Date" value={date} onChange={e => setDate(e.target.value)} />
         <input type="text" placeholder="Dropoff Point" value={dropoff} onChange={e => setDropoff(e.target.value)} />
         <input type="text" placeholder="Airline" value={airline} onChange={e => setAirline(e.target.value)} />
         <button onClick={handleSubmit}>Submit</button>
