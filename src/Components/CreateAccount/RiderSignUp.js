daa0fa7691ee2030cddc982d1d555ebbda53c157
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2023-09-27 20:30:56 -0500

    Adding log in page.

--- src/Components/CreateAccount/RiderSignUp.js
+++ src/Components/CreateAccount/RiderSignUp.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./RiderSignUp.css";
 
 function RiderSignUp() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const handleSubmit = () => {
     // TODO: handle when submit button is clicked
@@ -15,11 +15,36 @@ function RiderSignUp() {
   return (
     <div className="rider-signup-container">
       <h1>Rider Registration</h1>
-      <input type="text" placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} />
-      <input type="text" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} />
-      <input type="email" placeholder="Vanderbilt Email Address" value={email} onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-      <input type="tel" placeholder="Phone Number" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
+      <input
+        type="text"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Vanderbilt Email Address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="tel"
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
       <button onClick={handleSubmit}>Submit</button>
     </div>
   );
--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,4 +1,31 @@
+import React, { useState } from "react";
 import "./LogIn.css";
 
-function LogIn() {}
+function LogIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    // TODO: handle when submit button is clicked
+  };
+
+  return (
+    <div className="log-in-container">
+      <h1>Log In</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="Password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
 export default LogIn;
