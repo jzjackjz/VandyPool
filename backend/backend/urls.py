2cd64cc634b33ec7600eb853d60ac3443a5a3302
Jack Zhang <jackzhang@Jacks-MacBook-Pro-9.local>
2023-09-27 21:41:13 -0500

    created Accounts app in backend, used django RF for rider/driver models

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from Accounts.views import RegisterRiderView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/register/rider/', RegisterRiderView.as_view(), name="register_rider")
 ]
