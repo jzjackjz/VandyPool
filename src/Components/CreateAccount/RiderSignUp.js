cc82e01bf7f945d85beaee1b36d6b13cec874633
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2023-11-02 16:14:53 -0500

    Adding navigation changes.

--- src/Components/CreateAccount/RiderSignUp.js
+++ src/Components/CreateAccount/RiderSignUp.js
@@ -5,31 +5,30 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./RiderSignUp.css";
 
-const clientId = "889198131381-dhul247pghoitlna875j2t6kej68mllq.apps.googleusercontent.com";
+const clientId =
+  "889198131381-dhul247pghoitlna875j2t6kej68mllq.apps.googleusercontent.com";
 
 function RiderSignUp() {
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleGoogleRegister = async (response) => {
-    console.log(response)
+    console.log(response);
     if (response.error) {
       setError(`Google registration error: ${response.error}`);
       return;
     }
 
-    await axios.post('http://127.0.0.1:8000/auth/google-register', { token: response.credential })
-      .then(res => {
-        if (res.data.sessionToken) {
-          localStorage.setItem('sessionToken', res.data.sessionToken);
-          navigate('/');
-        } else {
-          setError('Registration failed: No session token returned from backend');
-        }
-      })
-      .catch(error => {
-        setError(`Error during Google registration: ${error.response.data.message}`);
+    try {
+      await axios.post("http://127.0.0.1:8000/auth/google-register", {
+        token: response.credential,
       });
+      navigate("/");
+    } catch (error) {
+      setError(
+        `Error during Google registration: ${error.response.data.message}`
+      );
+    }
   };
 
   return (
@@ -37,11 +36,8 @@ function RiderSignUp() {
       <h1>Rider Registration</h1>
       <p>Please use your Vanderbilt associated email</p>
       <GoogleLogin
-        clientId={clientId}
-        buttonText="Sign up with Google"
         onSuccess={handleGoogleRegister}
-        onFailure={handleGoogleRegister}
-        cookiePolicy={'single_host_origin'}
+        text="Sign Up With Google"
       />
     </div>
   );
