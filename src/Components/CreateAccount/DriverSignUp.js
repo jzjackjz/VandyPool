91fec722a65b7fd63827dd815e2fc6f7be6a5209
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2023-11-06 19:17:33 -0600

    Adding some logic for driver registration.

--- src/Components/CreateAccount/DriverSignUp.js
+++ src/Components/CreateAccount/DriverSignUp.js
@@ -1,33 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
+import { useAuth } from "../../AuthContext";
+import axios from "axios";
 import "./DriverSignUp.css";
 
 function DriverSignUp() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [carModel, setCarModel] = useState('');
-  const [carColor, setCarColor] = useState('');
-  const [licensePlate, setLicensePlate] = useState('');
-  const [seatsAvailable, setSeatsAvailable] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [carModel, setCarModel] = useState("");
+  const [carColor, setCarColor] = useState("");
+  const [licensePlate, setLicensePlate] = useState("");
+  const [seatsAvailable, setSeatsAvailable] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = () => {
-    // TODO: handle form submission
+    navigate("/");
   };
 
   return (
     <div className="driver-signup-container">
-      <h1>Driver Registration</h1>
-      <input type="text" placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} />
-      <input type="text" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} />
-      <input type="email" placeholder="Vanderbilt Email Address" value={email} onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-      <input type="tel" placeholder="Phone Number" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
-      <input type="model" placeholder="Car Model" value={carModel} onChange={e => setCarModel(e.target.value)} />
-      <input type="car_color" placeholder="Car Color" value={carColor} onChange={e => setCarColor(e.target.value)} />
-      <input type="license plate" placeholder="License Plate" value={licensePlate} onChange={e => setLicensePlate(e.target.value)} />
-      <input type="seatsAvailable" placeholder="Number of Available Seats" value={seatsAvailable} onChange={e => setSeatsAvailable(e.target.value)} />
+      <h1>Add A Few More Details Here</h1>
+      <input
+        type="text"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Vanderbilt Email Address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="tel"
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      <input
+        type="model"
+        placeholder="Car Model"
+        value={carModel}
+        onChange={(e) => setCarModel(e.target.value)}
+      />
+      <input
+        type="car_color"
+        placeholder="Car Color"
+        value={carColor}
+        onChange={(e) => setCarColor(e.target.value)}
+      />
+      <input
+        type="license plate"
+        placeholder="License Plate"
+        value={licensePlate}
+        onChange={(e) => setLicensePlate(e.target.value)}
+      />
+      <input
+        type="seatsAvailable"
+        placeholder="Number of Available Seats"
+        value={seatsAvailable}
+        onChange={(e) => setSeatsAvailable(e.target.value)}
+      />
       <button onClick={handleSubmit}>Submit</button>
     </div>
   );
--- src/Components/CreateAccount/DriverSignUpAccount.js
+++ src/Components/CreateAccount/DriverSignUpAccount.js
@@ -1,15 +1,16 @@
+import "./DriverSignUpAccount.css";
 import React from "react";
 import { useState } from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../AuthContext";
 import axios from "axios";
-import "./RiderSignUp.css";
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-function RiderSignUp() {
+function DriverSignUpAccount() {
   const [error, setError] = useState("");
+  const [duplicate, setDuplicate] = useState("");
   const { setIsAuthenticated } = useAuth();
   const navigate = useNavigate();
 
@@ -37,25 +38,32 @@ function RiderSignUp() {
         setError("Registration failed: No session token returned from backend");
       }
     } catch (error) {
-      setError(
-        `Error during Google registration: ${error.response.data.message}`
-      );
+      if (error.response.data.message == "User already exists") {
+        setDuplicate(
+          "It appears you already have an account, please proceed to add a few details"
+        );
+      } else {
+        setError(
+          `Error during Google registration: ${error.response.data.message}`
+        );
+      }
     }
   };
 
+  const handleSubmit = () => {
+    navigate("/DriverInfo");
+  };
+
   return (
-    <div className="rider-signup-container">
-      <h1>Rider Registration</h1>
+    <div className="driver-signup-container">
+      <h1>Driver Registration</h1>
       <p>Please use your Vanderbilt associated email</p>
-      <GoogleLogin
-        clientId={clientId}
-        onSuccess={handleGoogleRegister}
-        onFailure={handleGoogleRegister}
-        text="Sign Up With Google"
-      />
+      <GoogleLogin clientId={clientId} onSuccess={handleGoogleRegister} />
       {error && <div className="error-message">{error}</div>}
+      {duplicate && <div className="error-message">{duplicate}</div>}
+      {duplicate && <button onClick={handleSubmit}>Proceed Here</button>}
     </div>
   );
 }
 
-export default RiderSignUp;
+export default DriverSignUpAccount;
