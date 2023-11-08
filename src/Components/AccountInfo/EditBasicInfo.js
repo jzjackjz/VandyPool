2337d6acc91d1ba8fec3e72a54acc5e3f48fa505
jessicatang <jessica.h.tang@vanderbilt.edu>
2023-11-07 19:05:49 -0600

    Edit only allowed for phone and driver fields

--- src/Components/AccountInfo/EditBasicInfo.js
+++ src/Components/AccountInfo/EditBasicInfo.js
@@ -6,68 +6,104 @@ import { useNavigate } from "react-router-dom";
 function EditBasicInfo(){
     const navigate = useNavigate();
 
-    const [firstName, setFirstName] = useState("First");
-    const [lastName, setLastName] = useState("Last");
-
-    const [editedFirstName, setEditedFirstName] = useState(firstName);
-    const [editedLastName, setEditedLastName] = useState(lastName);
-
-    // Function to handle changes in the edited first name
-    const handleFirstNameChange = (e) => {
-        setEditedFirstName(e.target.value);
+    const [phone, setPhone] = useState("(615) 000-0000");
+    const [carModel, setCarModel] = useState("Car Model");
+    const [carColor, setCarColor] = useState("Car Color");
+    const [licensePlate, setLicensePlate] = useState("License Plate");
+  
+    const [editedPhone, setEditedPhone] = useState(phone);
+    const [editedCarModel, setEditedCarModel] = useState(carModel);
+    const [editedCarColor, setEditedCarColor] = useState(carColor);
+    const [editedLicensePlate, setEditedLicensePlate] = useState(licensePlate);
+  
+    // Function to handle changes in the edited phone number
+    const handlePhoneChange = (e) => {
+      setEditedPhone(e.target.value);
     };
-
-    // Function to handle changes in the edited last name
-    const handleLastNameChange = (e) => {
-        setEditedLastName(e.target.value);
+  
+    // Function to handle changes in the edited car model
+    const handleCarModelChange = (e) => {
+      setEditedCarModel(e.target.value);
     };
-
+  
+    // Function to handle changes in the edited car color
+    const handleCarColorChange = (e) => {
+      setEditedCarColor(e.target.value);
+    };
+  
+    // Function to handle changes in the edited license plate
+    const handleLicensePlateChange = (e) => {
+      setEditedLicensePlate(e.target.value);
+    };
+  
     // Function to save the changes
     const handleSave = () => {
-        // Update the actual first name and last name with the edited values when the user clicks Save
-        setFirstName(editedFirstName);
-        setLastName(editedLastName);
-
-        navigate("/AccountInfo");
-
-        // TO-DO: Update in the database
+      // Update the actual phone number, car model, car color, and license plate with the edited values when the user clicks Save
+      setPhone(editedPhone);
+      setCarModel(editedCarModel);
+      setCarColor(editedCarColor);
+      setLicensePlate(editedLicensePlate);
+  
+      navigate("/AccountInfo");
+  
+      // TO-DO: Update in the database
     };
-
+  
     // Function to cancel the changes
     const handleCancel = () => {
-        // Reset the edited first name and last name to the current values when the user clicks Cancel
-        setEditedFirstName(firstName);
-        setEditedLastName(lastName);
-
-        navigate("/AccountInfo");
+      // Reset the edited values to the current values when the user clicks Cancel
+      setEditedPhone(phone);
+      setEditedCarModel(carModel);
+      setEditedCarColor(carColor);
+      setEditedLicensePlate(licensePlate);
+  
+      navigate("/AccountInfo");
     };
 
     return (
         <div className="account_info_container">
             <bigbox>
-            
-                <subtitle>Edit Name</subtitle>
-                <div className="label">First Name</div>
-                <div className="input-box">
-                    <input
-                        type="text"
-                        className="name-input"
-                        value={editedFirstName}
-                        onChange={handleFirstNameChange}
-                    />
-                </div>
-                
+                <subtitle>Edit Information</subtitle>
 
-                <div className="label">Last Name</div>
+                <div className="label">Phone Number</div>
                 <div className="input-box">
-                    <input
-                        type="text"
-                        className="name-input"
-                        value={editedLastName}
-                        onChange={handleLastNameChange}
-                    />
+                <input
+                    type="text"
+                    className="name-input"
+                    value={editedPhone}
+                    onChange={handlePhoneChange}
+                />
                 </div>
-                
+
+                <div className="label">Car Model</div>
+                <div className="input-box">
+                <input
+                    type="text"
+                    className="name-input"
+                    value={editedCarModel}
+                    onChange={handleCarModelChange}
+                />
+                </div>
+
+                <div className="label">Car Color</div>
+                <div className="input-box">
+                <input
+                    type="text"
+                    className="name-input"
+                    value={editedCarColor}
+                    onChange={handleCarColorChange}
+                />
+                </div>
+
+                <div className="label">License Plate</div>
+                <div className="input-box">
+                <input
+                    type="text"
+                    className="name-input"
+                    value={editedLicensePlate}
+                    onChange={handleLicensePlateChange}
+                />
+                </div>
             </bigbox>
             <div className="button-container">
                 
