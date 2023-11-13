014013a8ddf1b227825a5b6560858b6ec0ea4615
Jack Zhang <jackjz1112@gmail.com>
2023-11-12 19:27:58 -0600

    moved backend to another repo

--- backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import FlightInformation
-
-# Register your models here.
-
-@admin.register(FlightInformation)
-class FlightInformationModel(admin.ModelAdmin):
-    list_filter = ('ride_type', 'flight_time', 'flight_date', 'dropoff_point', 'airline')
-    list_display = ('ride_type', 'flight_time', 'flight_date', 'dropoff_point', 'airline')
